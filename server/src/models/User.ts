8a09c2222554c0954d2d244e0229efcb2287f4c8
Mariel de Plata <126539030+mdeplata@users.noreply.github.com>
2025-10-17 15:55:28 +0000

    Setup MongoDB Connection

---
+++ server/src/models/User.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IUser extends Document {
+    email: string;
+    name: string;
+    avatar? : string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const UserSchema = new Schema<IUser>(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        avatar: {
+            type: String,
+            default: null,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export default mongoose.model<IUser>('User', UserSchema);
